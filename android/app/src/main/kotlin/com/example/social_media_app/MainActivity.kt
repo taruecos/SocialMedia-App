ea647187284b0151c3a2bfa24658eee8ea264347
taruecos <43816504+taruecos@users.noreply.github.com>
2023-07-13 23:07:17 +0200

    Update versions

--- android/app/src/main/kotlin/com/example/social_media_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/social_media_app/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.social_media_app
 
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterFragmentActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    private val CHANNEL = "yourChannelName"
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-        new MethodChannel (flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-        .setMethodCallHandler(
-            (call, result
-        ) -> {}
-        );
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                // Handle your method calls here.
+                // Use call.method to get method name.
+                // Use result.success(value) to return a value to Flutter.
+                // Use result.error(...) to return an error to Flutter.
+            }
     }
 }
